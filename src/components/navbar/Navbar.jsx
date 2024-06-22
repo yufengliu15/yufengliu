4e7223d2041d9c0d9deb8051bd86987ff6f86533
Yufeng Liu <yufeng.liu15@gmail.com>
2024-06-21 21:13:15 -0400

    fixed going from post to learning tab

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <ul className='navbar-menu'>
           <li><a href="/">Home</a></li>
           {/* <li><a href="project">Projects</a></li> */}
-          <li><a href="learning">Learning</a></li>
+          <li><a href="/learning">Learning</a></li>
           <li><a href="gallery">Gallery</a></li>
         </ul>
 
